fe2b333465a6d4cc601dd97361983a7528ab9011
eliseflaneuse <eliseflaneuse@gmail.com>
2023-05-29 18:52:59 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/index.';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
